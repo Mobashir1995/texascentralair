2bb055b2cf0ec4c47cabc0c5f6b8bbf9113c46c0
mobashir2762 <md.mobashir@selise.ch>
2023-06-20 12:37:34 +0600

    initi swiper js on review slider

---
+++ wp-content/themes/texascentralair/assets/js/theme.js
@@ -0,0 +1,7 @@
+jQuery( document ).ready( function() {
+    const swiper = new Swiper('.review-slider', {
+        // Default parameters
+        slidesPerView: 1,
+        autoplay: true
+      });
+} );
