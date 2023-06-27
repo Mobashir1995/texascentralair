2c71a5097c5d467a5bee396d6785aafa52d509f2
mobashir2762 <md.mobashir@selise.ch>
2023-06-27 22:44:32 +0600

    review masonry css

--- wp-content/themes/texascentralair/functions.php
+++ wp-content/themes/texascentralair/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'TEXAS_CENTRAL_AIR_CHILD_VERSION', '0.0.3.45' );
+define( 'TEXAS_CENTRAL_AIR_CHILD_VERSION', '0.0.3.46' );
 
 /**
  * After Setup Theme Hook
