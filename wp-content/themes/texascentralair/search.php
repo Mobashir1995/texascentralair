01ddfc4db56e08646d52d8f7beb6cde4017fbccc
mobashir2762 <md.mobashir@selise.ch>
2023-06-27 15:43:26 +0600

    search.php template

--- wp-content/themes/texascentralair/search.php
+++ wp-content/themes/texascentralair/search.php
@@ -1,17 +1,26 @@
-<?php
-    get_header();
-    echo '<h2>Displaying Search Result for <strong>' . $_GET['s'] . '</strong></h2>';
-    if( have_posts() ){
-        while( have_posts() ){
-            the_post();
+<?php get_header(); ?>
+    <div class="search-result-page-container">
+    <h2>Displaying Search Result for <strong><?php echo ucfirst($_GET['s']); ?></strong></h2>
+    <?php
+        if( have_posts() ){
+            while( have_posts() ){
+                the_post();
 ?>
-            <div class="tca-blog-grid">
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <div class="tca-blog-grid-excerpt"><h3><?php the_excerpt(); ?></h3></div>
-                <div class="tca-blog-grid-button"><a class="elementor-button" href="<?php the_permalink(); ?>">Read More</a></div>
-            </div>
+                <div class="tca-blog-grid">
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <div class="tca-blog-grid-excerpt"><h3><?php the_excerpt(); ?></h3></div>
+                    <div class="tca-blog-grid-button"><a class="elementor-button" href="<?php the_permalink(); ?>">Read More</a></div>
+                </div>
 <?php
-        }
-    }
-get_footer();
+            }
+            the_posts_pagination( array(
+                'prev_text' => __( 'Previous', 'twentyfifteen' ),
+                'next_text' => __( 'Next', 'twentyfifteen' ),
+                'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
+            ) );
+        }else{
 ?>
+            <h3>No Result for <strong><?php echo ucfirst($_GET['s']); ?></strong></h3>
+    <?php } ?>
+    </div>
+<?php get_footer(); ?>
