46d7abbe94705b7865888ee047d30a4b185f2a1d
mobashir2762 <md.mobashir@selise.ch>
2023-06-27 15:52:16 +0600

    404 template

---
+++ wp-content/themes/texascentralair/404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+    <div class="search-result-page-container">
+        <h2><strong>Not Found</strong></h2>
+        <h3>It looks like nothing was found at this location. Maybe try a search?</h3>
+
+    </div>
+<?php get_footer(); ?>
