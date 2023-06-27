c2dcfb44aa4b241d7c1a8789040e32fbb3b3b611
mobashir2762 <md.mobashir@selise.ch>
2023-06-27 16:48:45 +0600

    footer menu full on mobile

--- wp-content/themes/texascentralair/functions.php
+++ wp-content/themes/texascentralair/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'TEXAS_CENTRAL_AIR_CHILD_VERSION', '0.0.3.37' );
+define( 'TEXAS_CENTRAL_AIR_CHILD_VERSION', '0.0.3.38' );
 
 /**
  * After Setup Theme Hook
