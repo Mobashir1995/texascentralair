5753f30259065a2db72bb2fb5b05c6934c9a4a9c
mobashir2762 <md.mobashir@selise.ch>
2023-06-27 15:07:03 +0600

    form reset css

--- wp-content/themes/texascentralair/functions.php
+++ wp-content/themes/texascentralair/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'TEXAS_CENTRAL_AIR_CHILD_VERSION', '0.0.3.34' );
+define( 'TEXAS_CENTRAL_AIR_CHILD_VERSION', '0.0.3.35' );
 
 /**
  * After Setup Theme Hook
